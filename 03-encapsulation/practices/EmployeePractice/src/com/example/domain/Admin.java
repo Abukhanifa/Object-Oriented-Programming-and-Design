563e2af8d4b322f9fe5c596b0e45313aa7c84c2d
Abukhanifa <a_kuatuly@kbtu.kz>
2024-02-20 19:37:50 +0600

    added lab 4

---
+++ 03-encapsulation/practices/EmployeePractice/src/com/example/domain/Admin.java
@@ -0,0 +1,8 @@
+package com.example.domain;
+
+public class Admin extends Employee{
+    public Admin(int empId, String name, String ssn, double salary, String deptName){
+        super (empId, name, ssn, salary);
+    }
+
+}
